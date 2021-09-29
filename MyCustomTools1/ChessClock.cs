c8f9379aeb443d3b8113ab8dceb46b9a397eeef3
cipix37 <staicu.ciprian37@gmail.com>
2021-09-29 22:24:56 +0300

    PlayerTimer class now uses TImeSpan

--- MyCustomTools1/ChessClock.cs
+++ MyCustomTools1/ChessClock.cs
@@ -1,5 +1,8 @@
-﻿
+﻿using System;
+using System.Windows;
 using System.Timers;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace MyCustomTools1
 {
@@ -7,92 +10,153 @@ namespace MyCustomTools1
     //timer(default different thread) - pt sah
     //sau stopwatch(default same thread) - pt cub
 
-    public class ChessTimer
+    public class PlayerTimer : TextBox
     {
-        private readonly int DefaultHours, DefaultMinutes, DefaultSeconds; // DefaultSecondsPer10=0
-        private int Hours, Minutes, Seconds, SecondsPer10;
+        private TimeSpan Time, Increment, Interval, RedTextThreshold;
+        private readonly bool CountDown;
 
-        public ChessTimer(int hours, int minutes, int seconds) //can handle overflow
+        /// <summary>
+        /// Method for extracting actual time from the string input
+        /// </summary>
+        private void SpecialParse(object sender, RoutedEventArgs e)
         {
-            DefaultSeconds = seconds % 60;
-            minutes += seconds / 60;
-            DefaultMinutes = minutes % 60;
-            DefaultHours = hours + minutes / 60;
-            Reset();
+            // keep only specified characters
+            string FirstResult = System.Text.RegularExpressions.Regex.Replace(Text, "[^0-9:,+]", "");
+            // split string into time and increment
+            string[] SecondResult = FirstResult.Split('+'); // 0-Time 1-Increment
+            string Errors = "";
+            if (SecondResult.Length > 2) Errors = "Too many '+' characters in time format.";
+            else if (SecondResult.Length == 0) Errors = "No string found in time format.";
+            else
+            {
+                if (SecondResult.Length == 1)
+                {
+                    Increment = new TimeSpan();
+                }
+                else
+                {
+                    // extract increment from Firstresult[1];
+                }
+                // extract time from Firstresult[0];
+            }
+            MessageBox.Show(Errors);
         }
 
         /// <summary>
-        /// Actualizes Hours, Minutes, Seconds, SecondsPer10
+        /// Sets the TextBox text value to this.ToString(), but asynchronously
+        /// </summary>
+        private void SetTextAsync()
+        {
+            Dispatcher.Invoke(new Action(() =>
+            {
+                if (Time < RedTextThreshold) Foreground = Brushes.Red;
+                else Foreground = Brushes.Black;
+                Text = ToString();
+            }));
+        }
+
+        /// <summary>
+        /// Instantiate a new instance of the PlayerTimer class, specifying the count direction
+        /// </summary>
+        public PlayerTimer(bool countDown)
+        {
+            Time = new TimeSpan();
+            Increment = new TimeSpan();
+            Interval = new TimeSpan(0, 0, 0, 0, 100);
+            RedTextThreshold = new TimeSpan(0, 0, 30);
+            CountDown = countDown;
+
+
+            HorizontalContentAlignment = HorizontalAlignment.Center;
+            VerticalContentAlignment = VerticalAlignment.Center;
+            FontWeight = FontWeights.Bold;
+            LostFocus += new RoutedEventHandler(SpecialParse);
+        }
+
+        /// <summary>
+        /// Returns true if the time is zero (or lower, for certainty)
+        /// </summary>
+        public bool IsZero
+        {
+            get
+            {
+                return Time.TotalMilliseconds < 0;
+            }
+        }
+
+        /// <summary>
+        /// Returns the current number of ticks, necesarry for AI algorithm
+        /// </summary>
+        public int CurrentNumberOfTicks
+        {
+            get
+            {
+                return Convert.ToInt32(Time.TotalMilliseconds / Interval.TotalMilliseconds);
+            }
+        }
+
+        /// <summary>
+        /// Actualizes Time with the value of one Interval
         /// </summary>
         public void Tick()
         {
-            SecondsPer10--;
-            if (SecondsPer10 < 0)
-            {
-                SecondsPer10 = 9;
-                Seconds--;
-            }
-            if (Seconds < 0)
-            {
-                Seconds = 59;
-                Minutes--;
-            }
-            if (Minutes < 0)
-            {
-                Minutes = 59;
-                Hours--;
-            }
+            Time.Add(CountDown ? -Interval : Interval);
+            SetTextAsync();
         }
 
-        public void Set(int hours, int minutes, int seconds) //can handle overflow
+        /// <summary>
+        /// Sets the values for Time and Increment
+        /// </summary>
+        public void Set(TimeSpan time, TimeSpan increment)
         {
-            SecondsPer10 = 0;
-            Seconds = seconds % 60;
-            minutes += seconds / 60;
-            Minutes = minutes % 60;
-            Hours = hours + minutes / 60;
+            Time = time;
+            Increment = increment;
+            SetTextAsync();
         }
 
-        public void Reset()
+        /// <summary>
+        /// Increases internal time by increment.
+        /// </summary>
+        public void AddIncrement()
         {
-            Hours = DefaultHours;
-            Minutes = DefaultMinutes;
-            Seconds = DefaultSeconds;
-            SecondsPer10 = 0;
+            Time.Add(Increment);
+            SetTextAsync();
         }
 
-        public void Add(int hours, int minutes, int seconds) //can handle overflow
-        {
-            Seconds += seconds;
-            Minutes += minutes + Seconds / 60;
-            Hours += hours + Minutes / 60;
-            Seconds %= 60;
-            Minutes %= 60;
-        }
-
-        public string PrintDefaultValues()
-        {
-            return $"{DefaultHours}:{DefaultMinutes}:{DefaultSeconds}  ";
-        }
-
+        /// <summary>
+        /// Returns Time and Increment in clasic string format
+        /// </summary>
         public override string ToString()
         {
-            return $"{Hours}:{Minutes}:{Seconds},{SecondsPer10}";
+            string ToStringHelper(TimeSpan time)
+            {
+                string result = "";
+                if (time.Hours > 0) 
+                    result += time.Hours.ToString("D2") + ":";
+                if (time.Hours > 0 || time.Minutes > 0) 
+                    result += time.Minutes.ToString("D2") + ":";
+                if (time.Hours > 0 || time.Minutes > 0 || time.Seconds > 0)
+                    result += time.Seconds.ToString("D2") + "," + (time.Milliseconds / 100).ToString();
+                return result;
+            }
+            string increment = ToStringHelper(Increment);
+            if (increment == "") return ToStringHelper(Time);
+            return ToStringHelper(Time) + "+" + increment;
         }
     }
 
-    public class ChessClock
+    public class TimeControl
     {
         private readonly Timer MyTimer;
-        private readonly ChessTimer WhiteTimer, BlackTimer;
+        private readonly PlayerTimer WhiteTimer, BlackTimer;
         private bool CurrentPlayer;
-        public ChessTimer Clock(bool player)
+        public PlayerTimer Clock(bool player)
         {
             if (player) return WhiteTimer;
             return BlackTimer;
         }
 
-        public ChessTimer ActiveTimer
+        public PlayerTimer ActiveTimer
         {
             get
             {
@@ -101,13 +165,13 @@ namespace MyCustomTools1
             }
         }
 
-        public ChessClock()
+        public TimeControl()
         {
             CurrentPlayer = true;
             MyTimer = new Timer(100);
             MyTimer.Elapsed += new ElapsedEventHandler(Tick);
-            WhiteTimer = new ChessTimer(1, 5, 0);
-            BlackTimer = new ChessTimer(1, 5, 0);
+            WhiteTimer = new PlayerTimer(true);
+            BlackTimer = new PlayerTimer(true);
         }
 
         private void Tick(object sender, ElapsedEventArgs e)
